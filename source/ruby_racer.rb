58058d66f78024cfed62f83f551ab36a1f7f3311
John_and_Tony <tonyta.tt@gmail.com>
2014-02-20 16:25:56 -0600

    cleans up comments

--- source/ruby_racer.rb
+++ source/ruby_racer.rb
@@ -10,17 +10,11 @@ class RubyRacer
     @die = Die.new
   end
 
-  # Returns +true+ if one of the players has reached
-  # the finish line, +false+ otherwise
   def finished?
     players.has_value?(length)
-    # players.each {|_,position| return true if position >= 30}
-    # false
   end
 
-  # Returns the winner if there is one, +nil+ otherwise
   def winner
-    # players.key(length)
     winners = []
     players.each { |player, position| winners << player if position == length }
     if winners.length == 1
@@ -30,17 +24,11 @@ class RubyRacer
     end
   end
 
-  # Rolls the dice and advances +player+ accordingly
   def advance_player!(player)
     players[player] += die.roll
     players[player] = length if players[player] > length
-    # restrict to max length
   end
 
-  # Prints the current game board
-  # The board should have the same dimensions each time
-  # and you should use the "reputs" helper to print over
-  # the previous board
   def print_board
     puts "=" * length + "==="
     players.each do |player, position|
@@ -56,24 +44,18 @@ players = ['Tony', 'John', 'Kelsey', 'Dan', 'Better Dan']
 
 game = RubyRacer.new(players, 200)
 
-# This clears the screen, so the fun can begin
 clear_screen!
 
 until game.finished?
   players.each do |player|
-    # This moves the cursor back to the upper-left of the screen
     move_to_home!
 
-    # We print the board first so we see the initial, starting board
     game.print_board
     game.advance_player!(player)
-    # We need to sleep a little, otherwise the game will blow right past us.
-    # See http://www.ruby-doc.org/core-1.9.3/Kernel.html#method-i-sleep
     sleep(0.01)
   end
 end
 
-# The game is over, so we need to print the "winning" board
 move_to_home!
 game.print_board
 the_winner = game.winner
